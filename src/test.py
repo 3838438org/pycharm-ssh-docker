55f0b6b2bd95d42932ce487a78274322ef067625
1o0ko <wojciech.stokowiec@gmail.com>
2017-10-05 21:30:28 +0000

    added tensorflow to test script

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ This is a test for the dockerfile deployment
 '''
 import platform
 import subprocess
+import tensorflow as tf
 
 BASH_COMMAND = 'nvidia-smi'
 
@@ -15,3 +16,13 @@ if __name__ == '__main__':
 
     print(output)
     print(error)
+
+    # Tensorflow Test
+    ## Creates a graph.
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+    ## Creates a session with log_device_placement set to True.
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    ## Runs the op.
+    print(sess.run(c))
