e6f979a0c7c19c0851d1f8199f5aa1dbd87fce7c
1o0ko <wojciech.stokowiec@gmail.com>
2017-10-06 13:05:14 -0400

    changed the user name

--- bin/mac-connect.sh
+++ bin/mac-connect.sh
@@ -1,6 +1,6 @@
-USER=root
+REMOTE_USER=root
 HOST=<your-host>
 PASSWD=<your-passwd>
 PORT=8888
 
-sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $USER@$HOST -p $PORT
+sshpass -p $PASSWD ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $REMOTE_USER@$HOST -p $PORT
